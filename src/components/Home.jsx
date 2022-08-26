25acecc86d3d6a51f60b63632444d0a811c27764
Luiz Correa <luiz.correa@laborit.com.br>
2022-08-26 15:01:56 -0300

    second commit1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,9 @@
 export const Home = () => {
-    return <h1>home</h1>
+    return (
+        <div>
+            <h1>home</h1>
+            <a href="apolloturbo://redefinir-senha/senha">With scheme</a>
+            <a href="/redefinir-senha/senha">page</a>
+        </div>
+    )
 }
