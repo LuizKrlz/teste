e5851b8f68ebc4514f85452d6083764b6e601110
Luiz Correa <luiz.correa@laborit.com.br>
2022-08-26 14:15:57 -0300

    second commit

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import {
+    Routes as RoutesRouter,
+    Route,
+  } from "react-router-dom";
+
+import { Home } from './components/Home'
+import { RecoveryPassword } from './components/RecoveryPassword'
+
+export const Routes = () => {
+    return (
+        <RoutesRouter>
+            <Route path="/" element={<Home />} />
+            <Route path="/redefinir-senha/senha" element={<RecoveryPassword />} />
+        </RoutesRouter>
+    )
+}
---
+++ src/components/RecoveryPassword.jsx
@@ -0,0 +1,5 @@
+export const RecoveryPassword = () => {
+    return (
+        <h1>Recuperar senha</h1>
+    )
+}
