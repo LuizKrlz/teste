53a44a712b5181d9db6bbe002111aa484d4f7df3
Luiz Correa <luiz.correa@laborit.com.br>
2022-08-29 08:33:30 -0300

    second test

--- src/components/RecoveryPassword.jsx
+++ src/components/RecoveryPassword.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react"
+
 export const RecoveryPassword = () => {
+
+    useEffect(() => {
+        if (window !== undefined) {
+            window.location.replace("apolloturbo://redefinir-senha/senha?id=1u23y2187328173y21873y218&confirm_token=1623572163hagdfjaytsd7136126357")
+        }
+    }, [])
+
     return (
         <h1>Recuperar senha</h1>
     )
