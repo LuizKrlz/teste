310d4b48d314a441504b0a658a6736868a9f77b4
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2023-01-22 17:38:49 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ export const Home = () => {
     return (
         <div>
             <h1>home</h1>
-            <a href="apolloturbo://redefinir-senha/senha">With scheme</a>
+            <a href="portalmaisturbo://redefinir-senha/senha">With scheme</a>
             <a href="/redefinir-senha/senha">page</a>
         </div>
     )
